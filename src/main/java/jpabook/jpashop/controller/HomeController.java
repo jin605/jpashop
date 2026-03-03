e15af1c344107918fc39d32ac9868ca6e8dad0bc
jinddd3 <jinddd3@gmail.com>
2026-03-04 00:32:20 +0900

    feat : 홈 화면 구현 중

--- src/main/java/jpabook/jpashop/controller/HomeController.java
+++ src/main/java/jpabook/jpashop/controller/HomeController.java
@@ -1,6 +1,5 @@
 package jpabook.jpashop.controller;
 
-<<<<<<< HEAD
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,10 +15,4 @@ public class HomeController {
         log.info("home controller");
         return "home";
     }
-=======
-import org.springframework.stereotype.Controller;
-
-@Controller
-public class HomeController {
->>>>>>> b0175b21885c18726de6ca67e9798068d13720c8
 }
